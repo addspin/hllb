35f3a39bc9b2facb81b2ea4d577aa43289f8e8c5
tdv <denis.trofimov@reksoft.com>
2026-02-12 09:01:28 +0400

    up

--- utils/handleDNS.go
+++ utils/handleDNS.go
@@ -32,17 +32,50 @@ func HandleDNS(ctx context.Context, w dns.ResponseWriter, req *dns.Msg) {
 	// проверем вилдкард или нет ff.test.ru
 	testWildcard := strings.Split(queryNormDomainName, ".")
 	rootTest := strings.Join(testWildcard[1:], ".")
+	log.Println("TEST rootTest", rootTest)
 	log.Println("TEST testWildcard", testWildcard)
 
-	if len(testWildcard) > 2 {
+	if len(testWildcard) > 2 { //значит вилдкард
 		// проверем вилдкард запись в файле зоны *
 		if _, ok := Zone["*.test.ru"]; !ok {
 			resp.Rcode = dns.RcodeNameError
 			resp.Pack()
 			io.Copy(w, resp)
 		}
-		if rootTest == queryNormDomainName {
+		// если корневой домен верный и есть в зоне, тогда
+		if data, ok := Zone[rootTest]; ok {
+			switch qtype {
+			case dns.TypeA:
+				for _, ipData := range data.A {
+					log.Println("ADomain:", ipData)
+					ip, _ := netip.ParseAddr(ipData)
+					replyName := qyeryDomainName
+					if !strings.HasSuffix(replyName, ".") {
+						replyName += "."
+					}
+					a := &dns.A{
+						Hdr: dns.Header{Name: replyName, Class: dns.ClassINET, TTL: 3600},
+						A:   rdata.A{Addr: ip},
+					}
+					resp.Answer = append(resp.Answer, a)
+				}
+			case dns.TypeNS:
+				for _, nsDomain := range Zone["test.ru"].NS {
+					log.Println("nsDomain:", nsDomain)
+					ns := &dns.NS{
+						Hdr: dns.Header{Name: nsDomain, Class: dns.ClassINET, TTL: 3600},
+						NS:  rdata.NS{Ns: nsDomain},
+					}
+					resp.Answer = append(resp.Answer, ns)
+				}
+			}
 
+			if err := resp.Pack(); err != nil {
+				return
+			}
+			if _, err := io.Copy(w, resp); err != nil {
+				log.Printf("DNS write: %v", err)
+			}
 		}
 
 	}
@@ -78,54 +111,51 @@ func HandleDNS(ctx context.Context, w dns.ResponseWriter, req *dns.Msg) {
 		}
 		if _, err := io.Copy(w, resp); err != nil {
 			log.Printf("DNS write: %v", err)
+		} else {
+			resp.Rcode = dns.RcodeNameError
+			resp.Pack()
+			io.Copy(w, resp)
 		}
 	}
 
-	// test no exist *
-	if _, ok := Zone["*.test.ru"]; !ok {
-		resp.Rcode = dns.RcodeNameError
-		resp.Pack()
-		io.Copy(w, resp)
-	}
+	// if data, ok := Zone["*.test.ru"]; ok {
+	// 	// nsData := Zone["test.ru"]
+	// 	// log.Println("nsData", nsData)
+	// 	switch qtype {
+	// 	case dns.TypeA:
+	// 		for _, ipData := range data.A {
+	// 			log.Println("ADomain:", ipData)
+	// 			ip, _ := netip.ParseAddr(ipData)
+	// 			replyName := qyeryDomainName
+	// 			if !strings.HasSuffix(replyName, ".") {
+	// 				replyName += "."
+	// 			}
+	// 			a := &dns.A{
+	// 				Hdr: dns.Header{Name: replyName, Class: dns.ClassINET, TTL: 3600},
+	// 				A:   rdata.A{Addr: ip},
+	// 			}
+	// 			resp.Answer = append(resp.Answer, a)
+	// 		}
+	// 	case dns.TypeNS:
+	// 		for _, nsDomain := range Zone["test.ru"].NS {
+	// 			log.Println("nsDomain:", nsDomain)
+	// 			ns := &dns.NS{
+	// 				Hdr: dns.Header{Name: nsDomain, Class: dns.ClassINET, TTL: 3600},
+	// 				NS:  rdata.NS{Ns: nsDomain},
+	// 			}
+	// 			resp.Answer = append(resp.Answer, ns)
+	// 		}
+	// 	}
 
-	if data, ok := Zone["*.test.ru"]; ok {
-		// nsData := Zone["test.ru"]
-		// log.Println("nsData", nsData)
-		switch qtype {
-		case dns.TypeA:
-			for _, ipData := range data.A {
-				log.Println("ADomain:", ipData)
-				ip, _ := netip.ParseAddr(ipData)
-				replyName := qyeryDomainName
-				if !strings.HasSuffix(replyName, ".") {
-					replyName += "."
-				}
-				a := &dns.A{
-					Hdr: dns.Header{Name: replyName, Class: dns.ClassINET, TTL: 3600},
-					A:   rdata.A{Addr: ip},
-				}
-				resp.Answer = append(resp.Answer, a)
-			}
-		case dns.TypeNS:
-			for _, nsDomain := range Zone["test.ru"].NS {
-				log.Println("nsDomain:", nsDomain)
-				ns := &dns.NS{
-					Hdr: dns.Header{Name: nsDomain, Class: dns.ClassINET, TTL: 3600},
-					NS:  rdata.NS{Ns: nsDomain},
-				}
-				resp.Answer = append(resp.Answer, ns)
-			}
-		}
-
-		if err := resp.Pack(); err != nil {
-			return
-		}
-		if _, err := io.Copy(w, resp); err != nil {
-			log.Printf("DNS write: %v", err)
-		}
-	} else {
-		resp.Rcode = dns.RcodeNameError
-		resp.Pack()
-		io.Copy(w, resp)
-	}
+	// 	if err := resp.Pack(); err != nil {
+	// 		return
+	// 	}
+	// 	if _, err := io.Copy(w, resp); err != nil {
+	// 		log.Printf("DNS write: %v", err)
+	// 	}
+	// } else {
+	// 	resp.Rcode = dns.RcodeNameError
+	// 	resp.Pack()
+	// 	io.Copy(w, resp)
+	// }
 }
